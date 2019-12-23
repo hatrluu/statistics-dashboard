d10fafa37da7d36a0e0abf41721621b4cbac01ad
Patrick Petersen <ppetersen@atgworks.com>
2019-12-23 11:25:00 -0500

    Updated all loggers to use java.util.logging and not slf4j

--- src/main/java/com/cbi/dashboard/services/oam/OAMService.java
+++ src/main/java/com/cbi/dashboard/services/oam/OAMService.java
@@ -1,14 +1,14 @@
 package com.cbi.dashboard.services.oam;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public abstract class OAMService {
-	final static Logger logger = LoggerFactory.getLogger(OAMService.class);
+	final static Logger logger = Logger.getLogger(OAMService.class.getName());
 	
 	@Autowired
 	@Qualifier("oamJdbcTemplate")
--- src/main/java/com/cbi/dashboard/services/oim/OIMService.java
+++ src/main/java/com/cbi/dashboard/services/oim/OIMService.java
@@ -1,14 +1,14 @@
 package com.cbi.dashboard.services.oim;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public abstract class OIMService {
-	final static Logger logger = LoggerFactory.getLogger(OIMService.class);
+	final static Logger logger = Logger.getLogger(OIMService.class.getName());
 	
 	@Autowired
 	@Qualifier("oimJdbcTemplate")
